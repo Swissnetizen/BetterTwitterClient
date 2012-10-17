81837feb86376f39d162abe354d2bc066b10a754
Samarth's Dashboard <samu.agarwal@gmail.com>
2012-10-17 13:28:44 -0400

    Whoopee got onyx.Grabber showing!!

--- source/App.js
+++ source/App.js
@@ -1,6 +1,13 @@
 enyo.kind({
 	name: "App",
 	fit: true,
-	components: Components,
+    noStretch: true,
+    kind:"Panels",
+    classes:"app-panels, onyx",
+    arrangerKind: "CollapsingArranger",
+	components: [ {name: "Panel1", components: [ {kind:"onyx.MoreToolbar", components: [ {kind: "onyx.Grabber",}, {content: "Panel1"}, {kind:"onyx.Button", content:"hi"}],}],},
+     {name: "Panel2", components: [ {kind:"onyx.Toolbar", components: [ {kind: "onyx.Grabber",}, {content: "Panel2"}],}],},
+     {name: "Panel3", components: [ {kind:"onyx.MoreToolbar", components: [ {kind: "onyx.Grabber", noStretch: true, }, {content: "Panel3"}],}],}],
+    
 
 });
--- source/Components.js
+++ source/Components.js
@@ -1,1 +1,20 @@
-Components = [];
+Components = [
+		{kind: "FittableRows", noStretch: true, components: [
+
+			{kind: "onyx.Toolbar", components: [
+				{kind: "onyx.Grabber"}
+			]}
+		]},
+		{kind: "FittableRows", noStretch: true, components: [
+
+		    {kind: "onyx.Toolbar", components: [
+				{kind: "onyx.Grabber"}
+			]}
+		]},
+		{kind: "FittableRows", noStretch: true, components: [
+			
+		    {kind: "onyx.Toolbar", components: [
+				{kind: "onyx.Grabber"}
+			]}
+		]}
+	];
