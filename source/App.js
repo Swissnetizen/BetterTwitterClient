6c7589f1f1f7320e99c86d14dab4c2df18751e8d
Samarth's Dashboard <samu.agarwal@gmail.com>
2012-10-17 13:32:29 -0400

    Removed extra ]s

--- source/App.js
+++ source/App.js
@@ -5,9 +5,9 @@ enyo.kind({
     kind:"Panels",
     classes:"app-panels, onyx",
     arrangerKind: "CollapsingArranger",
-	components: [ {name: "Panel1", components: [ {kind:"onyx.MoreToolbar", components: [ {kind: "onyx.Grabber",}, {content: "Panel1"}, {kind:"onyx.Button", content:"hi"}],}],},
-     {name: "Panel2", components: [ {kind:"onyx.Toolbar", components: [ {kind: "onyx.Grabber",}, {content: "Panel2"}],}],},
-     {name: "Panel3", components: [ {kind:"onyx.MoreToolbar", components: [ {kind: "onyx.Grabber", noStretch: true, }, {content: "Panel3"}],}],}],
+	components: [  {name: "Panel1", components: [ {kind:"onyx.MoreToolbar", components: [ {kind: "onyx.Grabber",}, {content: "Panel1"}],}],},
+     {name: "Panel2", components: [ {kind:"onyx.MoreToolbar", components: [ {kind: "onyx.Grabber",}, {content: "Panel2"}],}],},
+     {name: "Panel3", components: [ {kind:"onyx.MoreToolbar", components: [ {kind: "onyx.Grabber",}, {content: "Panel3"}],}],}],
     
 
 });
